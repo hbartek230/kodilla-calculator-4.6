709f02fccf2ef4fcfbaaac2b4a029cbb00452544
hbartek230 <bhreniak@gmail.com>
2020-03-01 13:21:16 +0100

    dodano euklidesa do rozwiazania 4.6

--- src/main/java/com/kodilla/calculator/CalculatorApplication.java
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 @SpringBootApplication
 public class CalculatorApplication {
 
-    static double suma, roznica;
+    static double suma, roznica, nwd;
 
     public static void dodaj(double a, double b){
         suma = a + b;
@@ -16,6 +16,15 @@ public class CalculatorApplication {
 
     public static void odejmij(double a, double b){
         roznica = a-b;
+    }
+
+    public static void euklides(double a, double b){
+        while(b != 0) {
+            double c = a % b;
+            a = b;
+            b = c;
+        }
+        nwd = a;
     }
 
     public static void main(String[] args) {
@@ -31,6 +40,10 @@ public class CalculatorApplication {
         System.out.println("Suma wynosi: " + suma);
         odejmij(a, b);
         System.out.println("Różnica wynosi:  " + roznica);
+        euklides(a, b);
+        System.out.println("NWD wynosi: " + nwd);
+
+
     }
 
 }
